403ac419ceba09944879921e1a11a23bfbdeba12
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-21 19:10:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,36 +75,31 @@ function endGame() {
 }
 
 function moveDodger(e) {
-  const code = e.which
-
-  if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
-
-  if (code === LEFT_ARROW) {
+  e.preventDefault();
+  e.stopPropagation();
+  if(e.which === 37){
     moveDodgerLeft()
-  } else if (code === RIGHT_ARROW) {
+  }
+  else if (e.which === 39){
     moveDodgerRight()
   }
 }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame(function() {
-    const left = positionToInteger(DODGER.style.left)
-
-    if (left > 0) {
-      DODGER.style.left = `${left - 4}px`;
+  window.requestAnimationFrame(function(){
+    var left = positionToInteger($("#dodger").style.left)
+    if(left<180){
+      $("#dodger").style.left = '${left} -= 4 px'
     }
   })
+  }
 }
 
 function moveDodgerRight() {
-  window.requestAnimationFrame(function() {
-    const left = positionToInteger(DODGER.style.left)
-
-    if (left < 360) {
-      DODGER.style.left = `${left + 4}px`;
+  window.requestAnimationFrame(function(){
+    var left = positionToInterger($("#dodger").style.left)
+    if(left>180){
+      $("#dodger").style.left = '${left} += 4 px'
     }
   })
 }
