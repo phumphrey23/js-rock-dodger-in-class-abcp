cfda5a36986793cc70a5b91316dded1ab7285112
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-21 20:10:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -95,13 +95,13 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  // window.requestAnimationFrame(function(){
-  //   var dodger = document.getElementById("dodger")
-  //   var left = positionToInterger(DODGER.style.left)
-  //   if(left<360){
-  //     dodger.style.left = `${left + 4}px`
-  //   }
-  // })
+  window.requestAnimationFrame(function(){
+    var dodger = document.getElementById("dodger")
+    var left = positionToInterger(DODGER.style.left)
+    if(left<360){
+      dodger.style.left = `${left + 4}px`
+    }
+  })
 }
 
 function positionToInteger(p) {
